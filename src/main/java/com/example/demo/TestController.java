7470b1d19954a25501e4d09df9f1933d1307475c
prem kumar <155345459+premkumar2111@users.noreply.github.com>
2025-11-06 14:07:51 +0530

    Update test return message

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -10,6 +10,6 @@ public class TestController {
 
     @GetMapping
     public String test(){
-            return "this test by  prem kumar 21  32 21";
+            return "this test by  prem kumar 21";
     }
 }
